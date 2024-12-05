349862d5480bacb922d87c8dec52297baf14d04f
Subhan-Tanveer <subhanrider1978@gmail.com>
2024-12-05 18:35:34 +0500

    main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.title("🌦️ Rainfall Prediction App")
 st.markdown("**Predict whether it will rain based on weather conditions!** ☔")
 
 # Raw URL for the audio file hosted on GitHub
-audio_url = "https://raw.githubusercontent.com/username/repository/branch/path_to_file/mixkit-rain-and-thunder-storm-2390.wav"  # Replace with your raw GitHub URL
+audio_url = "mixkit-rain-and-thunder-storm-2390.wav"  # Replace with your raw GitHub URL
 
 # Streamlit audio component (handles audio more effectively)
 st.audio(audio_url, format='audio/wav', autoplay=True, loop=True)
